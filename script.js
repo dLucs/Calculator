a20f94dec012ea91b4b49a5d0e653534cf3a1af6
dLucs <lucas_61@hotmail.es>
2022-05-10 15:14:48 +0200

    added operation functionality to the calculator

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const calculator = document.querySelector(".calculator-grid");
 const keys = calculator.querySelector(".calculator-keys");
 
-// const previousOperand = document.querySelector('.previous-operand')
+const previousOperand = calculator.querySelector(".previous-operand");
 const currentOperand = calculator.querySelector(".current-operand");
 const output = calculator.querySelector(".output");
 
@@ -10,37 +10,65 @@ keys.addEventListener("click", (event) => {
   const keyValue = key.textContent;
   const displayValue = currentOperand.textContent;
   const type = key.dataset.type;
+  const previousKeyType = calculator.dataset.previousKeyType;
 
   // number key rule
-  if (type == "number")
+  if (type == "number") {
     if (displayValue == "0") {
+      currentOperand.textContent = keyValue;
+    } else if (previousKeyType == "operator") {
       currentOperand.textContent = keyValue;
     } else {
       currentOperand.textContent = displayValue + keyValue;
     }
+  }
 
   //operator key rule
   if (type == "operator") {
-    console.log(key);
+    //leave operator selected, and reset after new operator chosen
+    const currentActiveOperator = calculator.querySelector(
+      '[data-state="selected"]'
+    );
+    if (currentActiveOperator) {
+      currentActiveOperator.dataset.state = "";
+    }
 
-    calculator.dataset.previousKeyType = "operator";
+    key.dataset.state = "selected";
+
+    //save first value and operator
+    calculator.dataset.firstNumber = displayValue;
+    calculator.dataset.operator = key.dataset.key;
+    calculator.dataset.symbol = keyValue;
   }
+
+  if (type == "equals") {
+    const firstNumber = parseInt(calculator.dataset.firstNumber);
+    const operator = calculator.dataset.operator;
+    const secondNumber = parseInt(displayValue);
+    const symbol = calculator.dataset.symbol;
+
+    let result = "";
+
+    if (operator == "add") {
+      result = firstNumber + secondNumber;
+    }
+
+    if (operator == "subtract") {
+      result = firstNumber - secondNumber;
+    }
+
+    if (operator == "multiply") {
+      result = firstNumber * secondNumber;
+    }
+
+    if (operator == "divide") {
+      result = firstNumber / secondNumber;
+    }
+    //results on the display
+    currentOperand.textContent = result;
+    previousOperand.textContent =
+      firstNumber + " " + symbol + " " + secondNumber;
+  }
+  //reset previous keytype to in order to concatenate more than 1 number. this is a general rule instead of having to place it for "number", and "operator"
+  calculator.dataset.previousKeyType = type;
 });
-
-const add = function (a, b) {
-  return a + b;
-};
-
-const subtract = function (a, b) {
-  return a - b;
-};
-
-const multiply = function (a, b) {
-  return a * b;
-};
-
-const divide = function (a, b) {
-  return a / b;
-};
-
-const operate = function (operator, a, b) {};
